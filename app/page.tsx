92e5f2d26f95e2b2ac441f626449dc9722014f62
Joko Slamet <jokoslamet2207@gmail.com>
2024-03-08 18:16:17 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
         </svg>
         <div className="text-center max-w-screen-sm mb-10">
           <h1 className="text-stone-200 font-bold text-2xl">
-            Next.js + Postgres Auth Starter
+            Hello world
           </h1>
           <p className="text-stone-400 mt-5">
             This is a{' '}
